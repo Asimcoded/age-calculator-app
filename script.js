627b88372b0a71bed2c64c151298cb5740cf1f3e
Asim Das <asim71190@gmail.com>
2024-01-03 17:53:23 +0530

    Added checking date and giving output

--- script.js
+++ script.js
@@ -1,125 +1,108 @@
-let daysObj = [
-    {
-        month : 1,
-        days : 31
-    },
-    {
-        month : 2,
-        days : [28,29]
-    },
-    {
-        month : 3,
-        days : 31
-    },
-    {
-        month : 4,
-        days : 30
-    },
-    {
-        month : 5,
-        days : 31
-    },
-    {
-        month : 6,
-        days : 30
-    },
-    {
-        month : 7,
-        days : 31
-    },
-    {
-        month : 8,
-        days : 31
-    },
-    {
-        month : 9,
-        days : 30
-    },
-    {
-        month : 10,
-        days : 31
-    },
-    {
-        month : 11,
-        days : 30
-    },
-    {
-        month : 1,
-        days : 31
-    },
-    
-];
 let currentDate = new Date();
 let dayBox = document.getElementById("day");
 let monthBox = document.getElementById("month");
 let yearBox = document.getElementById("year");
 
-
-console.log(new Date().getDate());
-
 function emptyValidation() {
     let inputBoxs = document.getElementsByClassName("box");
-    for(i =0;i<inputBoxs.length;i++){
-        if(inputBoxs[i].value === undefined || inputBoxs[i].value === ""){
+    for (i = 0; i < inputBoxs.length; i++) {
+        if (inputBoxs[i].value === undefined || inputBoxs[i].value === "") {
             inputBoxs[i].nextElementSibling.style.display = "block";
             inputBoxs[i].nextElementSibling.textContent = "This field is required";
             inputBoxs[i].previousElementSibling.style.color = "hsl(0, 100%, 67%)";
             inputBoxs[i].style.borderColor = "hsl(0, 100%, 67%)";
         }
-        else{
+        else {
             inputBoxs[i].nextElementSibling.style.display = "none";
             inputBoxs[i].previousElementSibling.style.color = "hsl(0, 1%, 44%)";
             inputBoxs[i].style.borderColor = "hsl(0, 0%, 86%)";
         }
-    }    
-    
+    }
 }
-function dateValidation(){
-    let arr = [2024,11,7];
-    let daysVal,monthVal,yearVal;
-    if(arr[0] <= currentDate.getFullYear() && arr[0]  > 1850){
-        if(arr[1]<= 12){
-            if(arr[1] - 1 <=currentDate.getMonth()){
-                console.log("Month");
-                if(arr[2] >= currentDate.getDate()){
-                    daysVal = arr[2] - currentDate.getDate();
-                }
-                else{
-                    // let tempDay = arr[2] + cu
-                }
-            }
+
+function inputCheck() {
+    let year = parseInt(yearBox.value)
+    let month = parseInt(monthBox.value)
+    let day = parseInt(dayBox.value)
+    let inputBoxs = document.getElementsByClassName("box");
+    if (year <= currentDate.getFullYear()) {
+        yearBox.nextElementSibling.style.display = "none";
+        yearBox.previousElementSibling.style.color = "hsl(0, 1%, 44%)";
+        yearBox.style.borderColor = "hsl(0, 0%, 86%)";
+        if (month <= 12 && month > 0) {
+            monthBox.nextElementSibling.style.display = "none";
+            monthBox.previousElementSibling.style.color = "hsl(0, 1%, 44%)";
+            monthBox.style.borderColor = "hsl(0, 0%, 86%)";
+                    if (day <= new Date(year, month, 0).getDate()) {
+                        dayBox.nextElementSibling.style.display = "none";
+                        dayBox.previousElementSibling.style.color = "hsl(0, 1%, 44%)";
+                        dayBox.style.borderColor = "hsl(0, 0%, 86%)";
+                        displayCount(`${year}-${month}-${day}`)
+                    }
+                    else {
+                        dayBox.nextElementSibling.style.display = "block";
+                        dayBox.nextElementSibling.textContent = "Must be date month";
+                        dayBox.previousElementSibling.style.color = "hsl(0, 100%, 67%)";
+                        dayBox.style.borderColor = "hsl(0, 100%, 67%)";
+                    }
         }
-        else{
-            console.log("Enter the vaild month");
+        else {
+            monthBox.nextElementSibling.style.display = "block";
+            monthBox.nextElementSibling.textContent = "Must be valid month";
+            monthBox.previousElementSibling.style.color = "hsl(0, 100%, 67%)";
+            monthBox.style.borderColor = "hsl(0, 100%, 67%)";
         }
+    }
+    else {
+        yearBox.nextElementSibling.style.display = "block";
+        yearBox.nextElementSibling.textContent = "Must be in past";
+        yearBox.previousElementSibling.style.color = "hsl(0, 100%, 67%)";
+        yearBox.style.borderColor = "hsl(0, 100%, 67%)";
+    }
+}
+
+
+function displayCount(dateOfBirth) {
+    let brithDate = new Date(dateOfBirth);
+    let bd = brithDate.getDate();
+    let bm = brithDate.getMonth()+1;
+    let by = brithDate.getFullYear();
+
+    let today = new Date();
+    let cd = today.getDate();
+    let cm = today.getMonth()+1;
+    let cy = today.getFullYear();
+
+    let sd,sm,sy; //For storing the output
+
+    sy = cy-by;
+
+    if(cm>=bm){
+        sm = cm - bm;
     }
     else{
-        console.log("Ptu vaild date");
+        sy--;
+        sm = 12 + cm - bm;
     }
+
+    if(cd >= bd){
+        sd = cd - bd;
+    }
+    else{
+        sm--;
+        sd = new Date(by,bm,0).getDate() + cd -bd; 
+    }
+    if(sm<0){
+        sm = 11;
+        sy--;
+    }
+    document.getElementById("year-val").textContent = sy;
+    document.getElementById("month-val").textContent = sm;
+    document.getElementById("days-val").textContent = sd;
+    console.log(`You are ${sy} years, ${sm} months and ${sd} days`);
 }
-
-dateValidation()
-
-// function displayAge(date){
-//     let dob = new Date(date);
-
-//     let age = currentDate.getFullYear() - dob.getFullYear()  
-
-//     if(
-//         currentDate.getMonth() < dob.getMonth ||
-//         (currentDate.getMonth()==dob.getMonth() && currentDate.getDay()< dob.getDay())
-//     ){
-//         age--
-//     }
-
-//     console.log(age);
-// }
-
 let calculateBtn = document.querySelector("#btn");
-calculateBtn.addEventListener("click",()=>{
+calculateBtn.addEventListener("click", () => {
     emptyValidation();
-    dateValidation();
+    inputCheck();
 })
-
-
-// displayAge("1990-1-2")
